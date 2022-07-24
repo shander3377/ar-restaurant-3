c92c76a9ed1327813b3c04e8243393e55dc56607
Pankaj Agarwal <pankaj3377@gmail.com>
2022-07-24 18:12:48 +0530

    update

--- js/createMarker.js
+++ js/createMarker.js
@@ -12,6 +12,21 @@ AFRAME.registerComponent("create-marker", {
 
 			scene.appendChild(marker);
 
+			var date = new Date();
+			var todaysday = date.getDay();
+
+			var unavailable_Days = dish.unavailable_days;
+
+			if (unavailable_Days.includes(todaysday)) {
+				swal({
+					icon: "warning",
+					title: dish.dish_name.toUpperCase(),
+					text: "Dish Unavailable Today",
+					timer: "2500",
+					buttons: false,
+				});
+			}
+
 			var model = document.createElement("a-entity");
 			model.setAttribute("id", `model-${dish.id}`);
 			model.setAttribute("gltf-model", `url(${dish.model_url})`);
@@ -41,19 +56,59 @@ AFRAME.registerComponent("create-marker", {
 
 			plane.appendChild(titlePlane);
 
-            var dishTitle = document.createElement("a-entity")
-            dishTitle.setAttribute("position", {x: 0, y:0, z: 0.1})
-            dishTitle.setAttribute("text", {value: dish.dish_name.toUpperCase(), font: 'monoid', color: 'black', width: 1.8, height:1, align: "center"})
-            dishTitle.setAttribute("id", `dishTitle${dish.id}`)
+			var dishTitle = document.createElement("a-entity");
+			dishTitle.setAttribute("position", { x: 0, y: 0, z: 0.1 });
+			dishTitle.setAttribute("text", {
+				value: dish.dish_name.toUpperCase(),
+				font: "monoid",
+				color: "black",
+				width: 1.8,
+				height: 1,
+				align: "center",
+			});
+			dishTitle.setAttribute("id", `dishTitle${dish.id}`);
 
-            titlePlane.appendChild(dishTitle)
+			titlePlane.appendChild(dishTitle);
 
-            var dishIngredients = document.createElement("a-entity")
-            dishIngredients.setAttribute("position", {x: 0.3, y:0, z: 0.1})
-            dishIngredients.setAttribute("text", {value: `${dish.dish_ingredients.join("\n \n")}`, font: 'monoid', color: 'black', width: 2, align: "left"})
-            dishIngredients.setAttribute("id", `dishIngredients${dish.id}`)
+			var dishIngredients = document.createElement("a-entity");
+			dishIngredients.setAttribute("position", { x: 0.3, y: 0, z: 0.1 });
+			dishIngredients.setAttribute("text", {
+				value: `${dish.dish_ingredients.join("\n \n")}`,
+				font: "monoid",
+				color: "black",
+				width: 2,
+				align: "left",
+			});
+			dishIngredients.setAttribute("id", `dishIngredients${dish.id}`);
 
-            plane.appendChild(dishIngredients)
+			plane.appendChild(dishIngredients);
+
+			var pricePlane = document.createElement("a-image");
+			pricePlane.setAttribute("id", `price-plane-${dish.id}`);
+			pricePlane.setAttribute(
+				"src",
+				"https://raw.githubusercontent.com/whitehatjr/menu-card-app/main/black-circle.png"
+			);
+			pricePlane.setAttribute("width", 0.8);
+			pricePlane.setAttribute("height", 0.8);
+			pricePlane.setAttribute("position", { x: -1.3, y: 0, z: 0.3 });
+			pricePlane.setAttribute("rotation", { x: -90, y: 0, z: 0 });
+
+			//Price of the dish
+			var price = document.createElement("a-entity");
+			price.setAttribute("id", `price-${dish.id}`);
+			price.setAttribute("position", { x: 0.03, y: 0.05, z: 0.1 });
+			price.setAttribute("rotation", { x: 0, y: 0, z: 0 });
+			price.setAttribute("text", {
+				font: "mozillavr",
+				color: "white",
+				width: 3,
+				align: "center",
+				value: `Only\n $${dish.price}`,
+			});
+
+			pricePlane.appendChild(price);
+			marker.appendChild(pricePlane);
 		});
 	},
 	getDishes: async function () {
--- js/markerHandler.js
+++ js/markerHandler.js
@@ -1,19 +1,45 @@
 AFRAME.registerComponent("marker-handler", {
 	init: async function () {
-		this.el.addEventListener("markerFound", (e) => {
-			this.handlerMarkerFound(e);
-		});
+		    var dishes = await this.getDishes();
+
+    this.el.addEventListener("markerFound", () => {
+      var markerId = this.el.id;
+      this.handleMarkerFound(dishes, markerId);
+	});
+		
 		this.el.addEventListener("markerLost", (e) => {
 			this.handlerMarkerLost(e);
 		});
 	},
 
-	handlerMarkerFound: function () {
+	handlerMarkerFound: function (dishes, marker_id) {
 		var button_Div = document.getElementById("button-div");
 		button_Div.style.display = "flex";
 		var rating_button = document.getElementById("rating-button");
 		var order_button = document.getElementById("order-button");
-
+		var date = new Date()
+		var todaysday = date.getDay()
+		var days = [
+			"sunday",
+			"monday",
+			"tuesday",
+			"wednesday",
+			"thursday",
+			"friday",
+			"saturday"
+		  ];
+var dish = dishes.filter(dish => dish.id == marker_id)[0]
+		var unavailable_Days = dish.unavailable_days
+		
+		if (unavailable_Days.includes(todaysday)) {
+			swal({
+				icon: "warning",
+				title: dish.dish_name.toUpperCase(),
+				text: "Dish Unavailable Today",
+				timer: "2500",
+				buttons: false
+			});
+		}
 		rating_button.addEventListener("click", () => {
 			swal({
 				icon: "warning",
